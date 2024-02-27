df92ae72b4d1bf8f71d96028426eb713192038f2
Gu Ao Xiang <2194521087@hrbeu.edu.cn>
2024-02-27 21:40:15 +0800

    init

--- client/main.jsx
+++ client/main.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Meteor } from 'meteor/meteor';
 import { App } from '/imports/ui/App';
-
+import test from '/imports/mui/test'
 Meteor.startup(() => {
   const container = document.getElementById('react-target');
   const root = createRoot(container);
-  root.render(<App />);
+  // root.render(<App />);
 });
